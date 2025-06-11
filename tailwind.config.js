d9217d75e0f8dad37204b1e07364cf8027c549b5
Vaha <vaha@vaha.net>
2025-06-11 22:58:28 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+      colors: {
+        gold: "#D4AF37",
+      },
+    },
+  },
+  plugins: [],
+};
